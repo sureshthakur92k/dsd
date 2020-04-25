18dfed2bc67ba291ac577b84ab9b0bd614855393
Suresh Thakur <suresh.thakur92k@gmail.com>
2020-04-26 01:05:13 +0530

    Added new modal

---
+++ dsd/Models/TestMod.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dsd.Models
+{
+    public class TestMod
+    {
+        public int id { get; set; }
+    }
+}
